6d817ed6f140b95f1b800fcea9119388158a308e
Robby Chapman <robert.chapman@kronos.com>
2015-10-01 17:47:07 -0400

    Basic functionality for passing objects to UI for render. Using PrettyPrint.js for the meantime until I build one.

--- js/app/directives/analyzerDirectives.js
+++ js/app/directives/analyzerDirectives.js
@@ -12,6 +12,70 @@ angular.module('analyzerDirectivesModule', ['analyzerControllersModule'])
             restrict: 'E',
             templateUrl: '../../js/app/partials/monitorPartial.html',
             /* See js/app/controllers/analyzerControllers.js for WebSocketAnalyzerController implementation */
-            controller: 'WebSocketAnalyzerController'
+            controller: 'WebSocketAnalyzerController',
+            link: function(scope, element, attrs) {
+
+                var port;
+
+                scope.socketRequests = [];
+
+                scope.clearRequests = function () {
+
+                    scope.socketRequests = [];
+                };
+
+                function init() {
+
+                    createBackgroundPort();
+                    registerBackgroundEventHandler();
+                }
+
+                function createBackgroundPort() {
+
+                    port = chrome.runtime.connect({name: "SocketConnectionId"});
+                }
+
+                function canParseJSON(str) {
+                    try {
+                        JSON.parse(str);
+                    } catch (e) {
+                        return false;
+                    }
+                    return true;
+                }
+
+                function formatWebSocketFrame(frame) {
+
+                    if (frame.length > 0) {
+                        var tempFrame = frame.match(/"(.+)"/)[1];
+                        debugger;
+                        tempFrame = "\"" + tempFrame + "\"";
+                        if (canParseJSON(tempFrame)) {
+                            var messedUp = JSON.parse(JSON.parse(tempFrame));
+                            var val = (typeof messedUp == 'string') ? {'data': messedUp} : messedUp;
+                            appendNodeToDOM(val);
+                        }
+                    }
+                }
+
+                function registerBackgroundEventHandler() {
+
+                    if (!port) {
+                        createBackgroundPort();
+                    }
+                    port.onMessage.addListener(function (msg) {
+
+                        formatWebSocketFrame(msg);
+                    });
+                }
+
+                function appendNodeToDOM(node) {
+
+                    element.append(prettyPrint(node));
+                    //scope.$apply();
+                }
+
+                init();
+            }
         }
     });
